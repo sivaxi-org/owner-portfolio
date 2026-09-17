61dbcf3f2d342ce0b32bdf18e69c6f1ec641a8ea
ajaymalah <ajaymalah.2003@gmail.com>
2026-09-17 14:39:29 +0530

    feature/about section added

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../shared/components/navbar/navbar';
 import { Footer } from '../../shared/components/footer/footer';
 import { Hero } from '../sections/hero/hero';
+import { About } from '../sections/about/about';
 
 @Component({
-  imports: [Navbar, Footer, Hero],
+  imports: [Navbar, Footer, Hero,About],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
