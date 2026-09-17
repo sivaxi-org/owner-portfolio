41ac5d445a8785fe3c6fcbe2f34fcba39fe95dc9
ajaymalah <ajaymalah.2003@gmail.com>
2026-09-17 14:53:00 +0530

    feature/about section added

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,9 +3,15 @@ import { Navbar } from '../../shared/components/navbar/navbar';
 import { Footer } from '../../shared/components/footer/footer';
 import { Hero } from '../sections/hero/hero';
 import { About } from '../sections/about/about';
+import { Skills } from '../sections/skills/skills';
+import { Work } from '../sections/work/work';
+import { Experience } from '../sections/experience/experience';
+import { Blog } from '../sections/blog/blog';
+import { Testimonials } from '../sections/testimonials/testimonials';
+import { Contact } from '../sections/contact/contact';
 
 @Component({
-  imports: [Navbar, Footer, Hero,About],
+  imports: [Navbar, Footer, Hero, Skills, Work, About, Experience, Blog, Testimonials, Contact],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
