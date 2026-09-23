70ea586b0b05b004db99e2de5547fd72de2e4685
ajaymalah <ajaymalah.2003@gmail.com>
2026-09-23 11:03:27 +0530

    feature/about section added

---
+++ src/app/features/projects/projects.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-projects',
+  styleUrl: './projects.css',
+  templateUrl: './projects.html',
+})
+export class Projects {}
