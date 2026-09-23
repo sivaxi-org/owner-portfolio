f944efe9da72029396f9d4e248738a1021ed4f31
ajaymalah <ajaymalah.2003@gmail.com>
2026-09-23 14:37:32 +0530

    feature/about section added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
     {
       path: '',
       pathMatch: 'full',
-      redirectTo: 'portfolio/:username',
+      redirectTo: 'portfolio/ajaymalah',
     },
 
     {
